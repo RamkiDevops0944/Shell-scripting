7b02ae4ff435f65ddfc0dd43e8a6cb96560fa143
Ramki <ramakrishnan.venugopal@nokia.com>
2023-12-02 02:22:43 +0530

    test

--- Shebang.sh
+++ Shebang.sh
@@ -6,4 +6,4 @@ echo chekcing without double quote
 echo with and without doublequote are returing the same values
 
 
-echo -e "\e[31mRam \e[0m \n \e[32mShobi\e[0m \n\e[33mMithu\e[0m\n\e[36mYazhu\e[0m"
+echo -e "\e[31mRam \e[0m \n\e[32mShobi\e[0m \n\e[33mMithu\e[0m\n\e[36mYazhu\e[0m"
