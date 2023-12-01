04060cc643643d87c26706d35575a0ce7a81c2f1
Ramki <ramakrishnan.venugopal@nokia.com>
2023-12-02 02:16:06 +0530

    test

--- Shebang.sh
+++ Shebang.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # shebang notation 
-echo "[35m welcome to shell scripting [0m"
+echo -e "\e[35m welcome to shell scripting [0m"
 echo chekcing without double quote 
 echo with and without doublequote are returing the same values
 
